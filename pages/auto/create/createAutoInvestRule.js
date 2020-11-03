485f142961cd41899e15df7ded6cd0799930b026
Diamond <kimcuong24071996@gmail.com>
2020-11-03 23:04:33 +0700

    fix css

--- pages/auto/create/createAutoInvestRule.js
+++ pages/auto/create/createAutoInvestRule.js
@@ -1,5 +1,5 @@
 import MainLayout from '@layouts/main'
-import { PageHeader, Card, Input, Select, Button, Form, Row, Col } from 'antd';
+import { PageHeader, Card, Input, Select, Button, Form, Row, Col, Switch } from 'antd';
 import { useRouter } from 'next/router'
 import axios from '../../../configs/api-request';
 
@@ -18,50 +18,50 @@ export default function createAutoInvestRule() {
 
   const descriptionCustType = [
     {
-      desc:'Doanh nghiệp',
-      value:'DN'
+      desc: 'Doanh nghiệp',
+      value: 'DN'
     },
     {
-      desc:'Hộ kinh doanh',
-      value:'HKD'
+      desc: 'Hộ kinh doanh',
+      value: 'HKD'
     },
     {
-      desc:'Cá nhân',
-      value:'CN'
+      desc: 'Cá nhân',
+      value: 'CN'
     },
-    
+
   ];
 
   const descriptionSector = [
     {
-      desc:'Chọn tất cả',
-      value:'1'
+      desc: 'Chọn tất cả',
+      value: '1'
     },
     {
-      desc:'Thời trang, Phụ kiện, Trang sức',
-      value:'2'
+      desc: 'Thời trang, Phụ kiện, Trang sức',
+      value: '2'
     },
     {
-      desc:'Thực phẩm, Nhà hàng, Ăn uống',
-      value:'3'
+      desc: 'Thực phẩm, Nhà hàng, Ăn uống',
+      value: '3'
     }
-    
+
   ];
 
   const descriptionSurplus = [
     {
-      desc:'Có',
-      value:'1'
+      desc: 'Có',
+      value: '1'
     },
     {
-      desc:'Không',
-      value:'0'
+      desc: 'Không',
+      value: '0'
     }
-    
-    
+
+
   ];
   const descriptionOptionCustType = descriptionCustType.map(des => (<Select.Option value={des.value}>{des.desc}</Select.Option>))
-  const descriptionOptionSector = descriptionSector.map(des =>  (<Select.Option value={des.value}>{des.desc}</Select.Option>))
+  const descriptionOptionSector = descriptionSector.map(des => (<Select.Option value={des.value}>{des.desc}</Select.Option>))
   const descriptionOptionSurplus = descriptionSurplus.map(des => (<Select.Option value={des.value}>{des.desc}</Select.Option>))
   const route = useRouter();
   const onFinish = async (values) => {
@@ -78,6 +78,9 @@ export default function createAutoInvestRule() {
     }
   };
 
+  const switchButton = () => {
+    console.log('AAAAAAAAA')
+  }
   const onFinishFailed = errorInfo => {
     console.log('Failed:', errorInfo);
   };
@@ -89,14 +92,25 @@ export default function createAutoInvestRule() {
         title="Cài đặt"
         style={{ paddingLeft: 0 }}
       />
+      <Form>
+        <Form.Item
+          label="Cài đặt đầu tư tự động"
+          wrapperCol={{ span: 8 }}
+          style={{ fontWeight: "bold" }}
+        >
+          <Switch onClick={switchButton} />
+        </Form.Item>
+      </Form>
       <Card style={{ width: '100%', textAlign: "center" }}>
         <Form
-          layout="horizontal"
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+          formLayout="vertical"
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
         >
-          <Row>
-            <Col span="11" >
+          <Row gutter={48, 48}>
+            <Col span="12">
               <Form.Item
                 label="Chọn loại khách hàng"
                 name="loaiKhachHang"
@@ -105,21 +119,6 @@ export default function createAutoInvestRule() {
                   {descriptionOptionCustType}
                 </Select>
               </Form.Item>
-            </Col>
-            <Col span="11">
-              <Form.Item
-                label="Chọn ngành nghề"
-                name="chonNganhNghe"
-              >
-                <Select style={style.selectInput}>
-                  {descriptionOptionSector}
-                </Select>
-              </Form.Item>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col span="11">
               <Form.Item
                 title="Hạn mức tối đa theo số tiền đầu tư"
                 label="Nhập số tiền tối thiểu"
@@ -127,19 +126,6 @@ export default function createAutoInvestRule() {
               >
                 <Input suffix="VND" />
               </Form.Item>
-            </Col>
-            <Col span="11">
-              <Form.Item
-                label="Nhập số tiền tối đa"
-                name="soTienToiDa"
-              >
-                <Input suffix="VND" />
-              </Form.Item>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col span="11">
               <Form.Item
                 label="Sử dụng hết số dư"
                 name="suDungHetSoDu"
@@ -149,8 +135,21 @@ export default function createAutoInvestRule() {
                 </Select>
               </Form.Item>
             </Col>
-
-            <Col span="11">
+            <Col span="12">
+              <Form.Item
+                label="Chọn ngành nghề"
+                name="chonNganhNghe"
+              >
+                <Select style={style.selectInput}>
+                  {descriptionOptionSector}
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="Nhập số tiền tối đa"
+                name="soTienToiDa"
+              >
+                <Input suffix="VND" />
+              </Form.Item>
               <Form.Item
                 label="Hạn mức tối đa theo người gọi vốn"
                 name="hanMucToiDaTheoNguoiGoiVon"
@@ -160,37 +159,64 @@ export default function createAutoInvestRule() {
             </Col>
           </Row>
 
-          <Row>
-            <Col span="4">
+          <Row gutter={[48, 48]}>
+            <Col span="12">
               <Form.Item
-                label="Từ"
-                name="kiHanDauTuTu"
-              >
-                <Input suffix="tháng" />
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }} label="Cài đặt kì hạn đầu tư" >
+                <Row>
+                  <Col span={12} className="pr-3">
+                    <Form.Item
+                      labelCol={{ span: 3 }}
+                      wrapperCol={{ span: 21 }}
+                      label="Từ"
+                      labelAlign="left"
+                      name="kiHanDauTuTu"
+                    >
+                      <Input suffix="tháng" />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12} className="pl-3">
+                    <Form.Item
+                      labelCol={{ span: 3 }}
+                      wrapperCol={{ span: 21 }}
+                      label="Đến"
+                      labelAlign="left"
+                      name="kiHanDauTuDen" >
+                      <Input suffix="tháng" />
+                    </Form.Item>
+                  </Col></Row>
+              </Form.Item>
+            </Col>
+            <Col span="12" >
+              <Form.Item
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }} label="Cài đặt Lợi tức đầu tư" >
+                <Row>
+                  <Col span={12} className="pr-3">
+                    <Form.Item
+                      labelCol={{ span: 3 }}
+                      wrapperCol={{ span: 21 }}
+                      labelAlign="left"
+                      label="Từ"
+                      name="loiTucDauTuTu" >
+                      <Input suffix="%" />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12} className="pl-3">
+                    <Form.Item
+                      labelCol={{ span: 3 }}
+                      wrapperCol={{ span: 21 }}
+                      labelAlign="left"
+                      label="Đến"
+                      name="loiTucDauTuDen" >
+                      <Input suffix="%" />
+                    </Form.Item>
+                  </Col>
+                </Row>
               </Form.Item>
             </Col>
 
-            <Col span="4" style={{ marginLeft: '5%' }}>
-              <Form.Item
-                lable="Đến"
-                name="kiHanDauTuDen" >
-                <Input suffix="tháng" />
-              </Form.Item>
-            </Col>
-            <Col span="4" style={{ marginLeft: '5%' }}>
-              <Form.Item
-                lable="Từ"
-                name="loiTucDauTuTu" >
-                <Input suffix="%" />
-              </Form.Item>
-            </Col>
-            <Col span="5" style={{ marginLeft: '5%' }}>
-              <label style={{  float:'left', textAlign:'center' ,margin:'5%'}}> Đến</label>
-              <Form.Item
-                name="loiTucDauTuDen" >
-                <Input suffix="%" />
-              </Form.Item>
-            </Col>
           </Row>
           <Form.Item>
             <Button
