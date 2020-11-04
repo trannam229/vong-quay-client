6bbea381b7e0f425e9bb03cf1f77ab371653b5b5
Diamond <kimcuong24071996@gmail.com>
2020-11-04 22:43:13 +0700

    update code

--- pages/trade/invest.js
+++ pages/trade/invest.js
@@ -1,63 +1,111 @@
 import MainLayout from '@layouts/main';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from '../../configs/api-request';
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 import { get } from 'lodash';
 
+const columns = [
+  {
+    title: 'Tên chiến dịch',
+    dataIndex: 'ShortName',
+    key: 'ShortName',
+  },
+  {
+    title: 'Thông tin khách hàng',
+    dataIndex: 'InfoURL',
 
-const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
-  
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
-  
+    render: (item) => {
+      return (
+        <a href={item} target="_blank">Xem thông tin</a>);
+    }
+  },
+  // {
+  //   title: 'Loại KH',
+  //   dataIndex: 'ShortName',
+  //   key: 'ShortName',
+  // },
+  {
+    title: 'Hạng',
+    dataIndex: 'CustClass',
+    key: 'CustClass',
+  },
+  {
+    title: 'Kì hạn',
+    dataIndex: 'Term',
+    key: 'Term',
+    render: (term) => {
+      return <>{term} Tháng</>;
+    }
+  },
+  {
+    title: 'Lợi suất',
+    dataIndex: 'Int',
+    key: 'Int',
+    render: (int) => {
+      return <>{int}%</>
+    }
+  },
+  {
+    title: 'Ngành',
+    dataIndex: 'Sector',
+    key: 'Sector',
+  },
+  {
+    title: 'Số tiền kêu gọi đầu tư',
+    dataIndex: 'BRegAmt',
+    key: 'BRegAmt',
+  },
+  {
+    title: '% đã đầu tư',
+    dataIndex: 'InvestedRate',
+    key: 'InvestedRate',
+    render: (investedRate) => {
+      if (investedRate) {
+        return <>{investedRate}%</>
+      }
+      return <>-</>
+    }
+  },
+  {
+    title: 'Số tiền có thể đầu tư',
+    dataIndex: 'BRegRemain',
+    key: 'BRegRemain',
+  },
+  {
+    title: 'Trạng thái',
+    dataIndex: 'OrderType',
+    key: 'OrderType',
+  },
+  {
+    title: '',
+    key: 'action',
+    render: (item) => {
+      return <Button>Đầu tư ngay</Button>
+    }
+  },
+];
+
 
 export default function Example() {
-    useEffect(() => {
-        const fetchData = async () => {
-            const { data: dellToSell } = await axios.get("/deal-to-sell");
-            const { data: priceBoard } = await axios.get("/price-board");
-            const dataDellToSell = get(dellToSell, ['GetDealToSellResult', 'DealInfoList', 'DealInfo'], []);
-            const dataPriceBoard = get(priceBoard, ['GetPriceBoardResult', 'PriceBoardList', 'PriceBoard'], []);
-            console.log(dataPriceBoard.concat(dataDellToSell));
-            console.log(dataPriceBoard);
-            console.log(dataDellToSell);
-        }
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: dellToSell } = await axios.get("/deal-to-sell");
+      const { data: priceBoard } = await axios.get("/price-board");
+      const dataDellToSell = get(dellToSell, ['GetDealToSellResult', 'DealInfoList', 'DealInfo'], []);
+      const dataPriceBoard = get(priceBoard, ['GetPriceBoardResult', 'PriceBoardList', 'PriceBoard'], []);
+      setItems(dataPriceBoard.concat(dataDellToSell));
+      
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <MainLayout>
-            <Table dataSource={dataSource} columns={columns} />;
+  return (
+    <MainLayout>
+      <Table dataSource={items} columns={columns} />;
 
-        </MainLayout>
-    )
+    </MainLayout>
+  )
 }
