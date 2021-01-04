e28cf6130e5ba4f5dd69ba786cb261b4ade241a7
Diamond <kimcuong24071996@gmail.com>
2021-01-04 14:40:24 +0700

    revert port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const next = require("next");
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
