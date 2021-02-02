361d6dcd97d48441e9111fcaa08efbbc7b639804
Diamond <kimcuong24071996@gmail.com>
2021-02-02 10:25:41 +0700

    done menu top

--- layouts/main.js
+++ layouts/main.js
@@ -1,47 +1,60 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import Cookies from 'js-cookie';
 import RouterConfigs from "../config-router";
-import { useEffect } from 'react';
+import {useEffect} from 'react';
+
 const ProLayout = dynamic(() => import('@ant-design/pro-layout'), {
-  ssr: false,
+    ssr: false,
 })
 
 
 const menuHeaderRender = (logoDom, titleDom, props) => (
-  <Link href="/" className="m-auto">
-    <div className="text-center">{logoDom}</div>
-  </Link>
+    <Link href="/" className="m-auto">
+        <div className="text-center">{logoDom}</div>
+    </Link>
 )
 
 const menuItemRender = (options, element) => (
-  <Link href={options.path}>
-    <a>{element}</a>
-  </Link>
+    <Link href={options.path}>
+        <a>{element}</a>
+    </Link>
 )
 
+const headerRender = (config) => {
+    const user = Cookies.get('account_name');
 
-export default function Main({ children }) {
-  const route = useRouter();
-  useEffect(() => {
-    const token = Cookies.get('access_token');
-    if (!token) {
-      route.push({ pathname: '/login' })
-      return;
-    }
-  }, [])
+    return (
+        <div className="d-flex hello-user justify-content-end mr-5">
+            Xin chào nhà đầu tư {user}
+            <img src="/vietnam.svg" className="ml-3" width="32"/>
+            <img src="/united-kingdom.svg" className="ml-3" width="32"/>
+        </div>
+    )
+}
 
-  return (
-    <ProLayout
-      style={{ minHeight: '100vh' }}
-      route={RouterConfigs}
-      logo="/logo-white.png"
-      menuItemRender={menuItemRender}
-      menuHeaderRender={menuHeaderRender}
-      title="Lendbiz"
-    >
-      {children}
-    </ProLayout>
-  )
+export default function Main({children}) {
+    const route = useRouter();
+    useEffect(() => {
+        const token = Cookies.get('access_token');
+        if (!token) {
+            route.push({pathname: '/login'})
+            return;
+        }
+    }, [])
+
+    return (
+        <ProLayout
+            style={{minHeight: '100vh'}}
+            route={RouterConfigs}
+            logo="/logo-white.png"
+            menuItemRender={menuItemRender}
+            menuHeaderRender={menuHeaderRender}
+            title="Lendbiz"
+        >
+            {headerRender()}
+            {children}
+        </ProLayout>
+    )
 }
